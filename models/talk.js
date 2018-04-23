a24b9a78ad2d27d43df096160b7ae18f902ca0be
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-23 14:51:20 +1200

    set up database schema

---
+++ models/talk.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const talkSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+})
+
+const Talk = mongoose.model('Talk', talkSchema)
+
+module.exports = Talk
