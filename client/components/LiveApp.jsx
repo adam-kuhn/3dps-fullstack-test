417d2d8f3f8184b803471ea1133f639dd8b6e68e
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-23 22:24:20 +1200

    remove button

--- client/components/LiveApp.jsx
+++ client/components/LiveApp.jsx
@@ -4,25 +4,12 @@ import {connect} from 'react-redux'
 
 import {requestTalks} from '../actions'
 class LiveApp extends React.Component {
-  // constructor () {
-  //   super()
-  // this.clickHandler = this.clickHandler.bind(this)
-  // }
   componentDidMount () {
     this.props.dispatch(requestTalks())
   }
-  // clickHandler () {
-  //   request
-  //     .get('/api/v1/talks')
-  //     .set('Content-Type', 'application/json')
-  //     .send()
-  //     .then(res => {
-  //       console.log(res.body)
-  //     })
-  // }
   render () {
     return (
-      <button type='button' onClick={this.clickHandler}>api test</button>
+      <h1>Lightning Talks</h1>
     )
   }
 }
