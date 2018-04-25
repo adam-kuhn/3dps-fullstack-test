79855405c51cc8e8f7d6d9a5dc47697cb186095f
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-25 17:48:36 +1200

    sort talks based on vote

--- client/reducers/talks.js
+++ client/reducers/talks.js
@@ -1,13 +1,22 @@
 import {RECEIVED_TALKS, RECEIVED_VOTES} from '../actions/talks'
 
+function compare (a, b) {
+  if (a.votes > b.votes) {
+    return -1
+  }
+  if (a.votes < b.votes) {
+    return 1
+  }
+  return 0
+}
+
 function talks (state = [], action) {
   switch (action.type) {
     case (RECEIVED_TALKS): {
       return action.talks
     }
     case (RECEIVED_VOTES): {
-      console.log('vote talks', action.talks)
-      return action.talks
+      return action.talks.sort(compare)
     }
     default:
       return state
